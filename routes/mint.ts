ada76ae22bff370776c11a6ad6731359887c53a4
Vickaiah <118375352+cryptowithvickaiah@users.noreply.github.com>
2022-11-16 18:12:08 +0100

    Update mint.ts

--- routes/mint.ts
+++ routes/mint.ts
@@ -1,18 +1,6 @@
 import express from 'express';
-import { calcRoute } from './calcRoute';
-import { defaultRoute } from './defaultRoute';
-import { mint } from './minting';
 import { account } from './account';
-import { collection } from './collection';
-import { create_col } from './create_collectiong';
-import { confirm_txn } from './confirm_txn';
 
 export const routes = express.Router();
 
-routes.use(defaultRoute);
-routes.use(calcRoute);
-routes.use(mint);
 routes.use(account);
-routes.use(collection);
-routes.use(create_col);
-routes.use(confirm_txn);
